f46a684707bdc63bf2b21782149dc2eb8df99499
Luke Merrick <luke.merrick@snowflake.com>
2023-10-05 17:10:05 -0700

    Strip out fast lane priority from the API

--- service_api/api.py
+++ service_api/api.py
@@ -9,7 +9,6 @@ from typing import Optional
 import lodis.hashmap
 import lodis.small_priority_queue
 import numpy as np
-from sfc_lru_cache import LRUCache
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.routing import Route
@@ -48,7 +47,6 @@ QUERY_ID_CACHE_MAXSIZE = 10_000
 _job_id_counter: Optional[JobIdCounter] = None
 _input_queue: Optional[lodis.small_priority_queue.SmallPriorityQueue] = None
 _result_map: Optional[lodis.hashmap.HashMap] = None
-_query_id_cache: Optional[LRUCache] = None
 
 
 #### Global state initialization.
@@ -56,11 +54,10 @@ def setup_state(
     embedding_input_queue_spec: lodis.small_priority_queue.SmallPriorityQueueSpec,
     embedding_result_map_spec: lodis.hashmap.HashMapSpec,
 ) -> None:
-    global _job_id_counter, _input_queue, _result_map, _query_id_cache
+    global _job_id_counter, _input_queue, _result_map
     assert _job_id_counter is None
     assert _input_queue is None
     assert _result_map is None
-    assert _query_id_cache is None
 
     # Initialize the job id counter.
     logger.info("Initializing job id counter")
@@ -68,7 +65,6 @@ def setup_state(
 
     # Initialize query id cache.
     logger.info("Initializing query id cache")
-    _query_id_cache = LRUCache(maxsize=QUERY_ID_CACHE_MAXSIZE)
 
     # Open the input queue and result hashmap.
     logger.info("Opening embedding input queue and result map")
@@ -95,11 +91,10 @@ def _array_to_base64_float32(array: np.ndarray) -> B64EncodedFloat32Vector:
 #### API routes.
 async def embed(request: Request) -> JSONResponse:
     # We know these are initialized, but the type checker doesn't.
-    global _job_id_counter, _input_queue, _result_map, _query_id_cache
+    global _job_id_counter, _input_queue, _result_map
     assert isinstance(_job_id_counter, JobIdCounter)
     assert isinstance(_input_queue, lodis.small_priority_queue.SmallPriorityQueue)
     assert isinstance(_result_map, lodis.hashmap.HashMap)
-    assert isinstance(_query_id_cache, LRUCache)
 
     logger.debug("Handling embedding")
 
@@ -109,18 +104,11 @@ async def embed(request: Request) -> JSONResponse:
     batch_id = request.headers.get(SF_QUERY_BATCH_ID_HEADER, "UNKNOWN_BATCH")
     row_numbers, texts = zip(*request_json["data"])
 
-    # Check if this batch qualifies for priority embedding by being the first
-    # batch of a query and by being small.
-    is_seen_before_query_id = _query_id_cache.get(query_id, default=False)
-    if not is_seen_before_query_id and len(texts) <= LARGEST_PRIORITY_BATCH_SIZE:
-        priority = lodis.small_priority_queue.MOST_URGENT_PRIORITY
-    else:
-        priority = lodis.small_priority_queue.LEAST_URGENT_PRIORITY
+    # We could add logic to parse the request and determine priority here, but
+    # for simplicity we'll just prioritize everything the same.
+    priority = lodis.small_priority_queue.LEAST_URGENT_PRIORITY
 
-    # Add/bump-to-latest the query id.
-    _query_id_cache[query_id] = True
-
-    # Queue up embedding.
+    # Queue up the texts for embedding.
     logger.info(
         f"[Query {query_id} | Batch {batch_id}] "
         f"Queueing embedding jobs at priority {priority}"
@@ -135,23 +123,18 @@ async def embed(request: Request) -> JSONResponse:
         _input_queue.put_batch_nowait(job_inputs, priority=priority)
     except lodis.small_priority_queue.NotEnoughSpace:
         # If the queue is full, the service is overwhelmed, so let's return 429.
-        # NOTE: This can unfortunately break the "fast lane" priority treatment.
-        # TODO: Consider moving to two separate queues to reserve more space for
-        # priority queries.
         return JSONResponse(content="", status_code=429)
 
-    # Wait for results.
+    # Wait for results from the embedding loop.
     results = []
     for job_id in job_ids:
         embedding_vector = await _result_map.pop_block_async(job_id)
         embedding_b64_encoded = _array_to_base64_float32(embedding_vector)
         results.append(embedding_b64_encoded)
-
     assert len(row_numbers) == len(results)
 
     # Format a JSON response satisfying external function response structure.
     response_json = {"data": list(zip(row_numbers, results))}
-
     return JSONResponse(response_json)
 
 
